ffffe32a86314639a8498baefd6d2f040e5d90b5
rnick1 <rnick0260@gmail.com>
2021-04-17 14:22:06 -0700

    Added html routes.

--- routes/html.js
+++ routes/html.js
@@ -1,30 +1,25 @@
-// exercise.html route
-// index.html route
-// stats.html route
-// ---------------------------------------------
-// FROM WEEK 11 ACTIVITY 15:
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
-
 const path = require('path');
 
 // ROUTING
-
 module.exports = (app) => {
   // => HTML GET Requests
-  // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
-
-  app.get('/tables', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/tables.html'));
+  app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
   });
 
-  app.get('/reserve', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/reserve.html'));
+  app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+  app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
   });
 
   // If no matching route is found default to home
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/home.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 };
